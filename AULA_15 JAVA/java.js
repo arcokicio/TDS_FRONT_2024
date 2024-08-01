8c1bd55c9ff6e2644e446bec53734435c830ea5d
arcokicio <151792143+arcokicio@users.noreply.github.com>
2024-07-31 22:07:53 -0300

    Add files via upload

---
+++ AULA_15 JAVA/java.js
@@ -0,0 +1,6 @@
+const horizontais = document.getElementsByClassName("celula");
+const back_horizontais = document.getElementsByClassName("back_cel");
+
+if (horizontais[.back_cel].innerHTML == ""){
+horizontais[celula].innerHTML = 
+}
