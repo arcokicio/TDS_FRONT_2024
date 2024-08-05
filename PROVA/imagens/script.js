86e00ba789d8ead8f7e5935e4ae50e4323628be4
arcokicio <151792143+arcokicio@users.noreply.github.com>
2024-08-05 20:18:19 -0300

    Add files via upload
    entrega prova

---
+++ PROVA/imagens/script.js
@@ -0,0 +1,29 @@
+const nome = window.document.getElementById("nome")
+const email = window.document.getElementById("email")
+const ol = window.document.createElement("ul");
+const li = window.document.createElement("li");
+
+
+function SalvaCadastro(){
+    const input = window.document.getElementById("nome, idade");
+
+    nome.push(
+        {
+            nome: input.nome,
+            email: input.email
+        }
+    );
+
+    }
+
+function*(){
+    alunos.forEach((SalvaCadastro)=>{
+        li.innerHTML = `${mensagem_confirmacao}, Senhor:${body.nome} acabamos de enviar um email para:${body.email}`
+        ol.appendChild(li);
+    });
+    }
+    
+    console.log(SalvaCadastro);
+    body.appendChild(ul);
+    
+    
